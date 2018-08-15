9d9bdb47da1415157b9023d9f1c921709712120b
Bill_王彪_068 <bill@hxonline.tv>
2018-08-15 14:08:57 +0800

    init

---
+++ src/main/java/com/hw/controller/HwController.java
@@ -0,0 +1,21 @@
+package com.hw.controller;
+
+import com.hw.dto.message.Message;
+import com.hw.dto.message.MessageType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/5/25 0025.
+ */
+@RestController
+public class HwController {
+    @RequestMapping(value = "/hw", method = RequestMethod.GET)
+    public ResponseEntity<Message> getHw() {
+        String res = "Hello World !";
+        return new ResponseEntity<Message>(new Message(MessageType.MSG_TYPE_SUCCESS, (Object) res), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/hw/dto/message/MessageType.java
@@ -0,0 +1,6 @@
+package com.hw.dto.message;
+
+public class MessageType {
+    public static final String MSG_TYPE_SUCCESS="SUCCESS";
+    public static final String MSG_TYPE_ERROR="ERROR";
+}
